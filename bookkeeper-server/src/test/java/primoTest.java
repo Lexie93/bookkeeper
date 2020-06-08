ef94cda30a1302d4d06dd05145408907e5d3d518
Lexie <ponzo93@gmail.com>
2020-06-08 20:11:29 +0200

    first multimodule sonar test

---
+++ bookkeeper-server/src/test/java/primoTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.fail;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class primoTest {
+
+@Test
+public void test(){
+	Assert.assertTrue(true);
+}
+}
