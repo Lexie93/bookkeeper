73601dbfb05b798408df3ad4e604aed186261888
Lexie <ponzo93@gmail.com>
2020-06-10 02:26:07 +0200

    added equals method test and some tests ignored

--- tests/src/test/java/ValueTest.java
+++ tests/src/test/java/ValueTest.java
@@ -1,6 +1,7 @@
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -50,8 +51,8 @@ public class ValueTest {
 				}
 				return set;
 			}
-			
 		});
+		Mockito.lenient().when(mockedField.size()).thenReturn(fields.length);
 	}
 	
 	@Test
@@ -89,6 +90,71 @@ public class ValueTest {
 		}
 		str += "]";
 		Assert.assertEquals(value.toString(), str);
+	}
+	
+	@SuppressWarnings("unlikely-arg-type")
+	@Test
+	public void testEqualsNotSameInstance() {
+		String obj = "wrongType";
+		Assert.assertFalse(value.equals(obj));
+	}
+	
+	@Test
+	public void testEqualsNotSameSize() {
+		boolean cond1 = value.equals(null);
+		Value val = new Value();
+		val.setField("field", "value".getBytes());
+		
+		Mockito.when(mockedField.size()).thenReturn(2);
+		
+		boolean cond2 = value.equals(val);
+		Assert.assertTrue(!cond1 && !cond2);
+	}
+	
+	@Ignore
+	@Test
+	public void testEqualsNotSameEntries1() {
+		Value val = new Value();
+		for(int i=0; i<fields.length-1; i++) {
+			if (values[i] != null)
+				val.setField(fields[i], values[i].getBytes());
+			else
+				val.setField(fields[i], null);		
+			}
+		val.setField("differentField", "differentBytes".getBytes());
+		Assert.assertFalse(value.equals(val));
+	}
+	
+	@Ignore
+	@Test
+	public void testEqualsNotSameEntries2() {
+		Value val1 = new Value();
+		Value val2 = new Value();
+		val1.setField("field", "value".getBytes());
+		val2.setField("differentField", "differentValue".getBytes());
+		Assert.assertFalse(val1.equals(val2));
+	}
+	
+	@Ignore
+	@Test
+	public void testEqualsTrue1() {
+		Value val = new Value();
+		for(int i=0; i<fields.length; i++) {
+			if (values[i] != null)
+				val.setField(fields[i], values[i].getBytes());
+			else
+				val.setField(fields[i], null);
+		}
+		Assert.assertTrue(value.equals(val));
+	}
+	
+	@Test
+	public void testEqualsTrue2() {
+		Value val1 = new Value();
+		Value val2 = new Value();
+		val1.setField("sameField", "sameValue".getBytes());
+		val2.setField("sameField", "sameValue".getBytes());
+		Assert.assertTrue(val1.equals(val2));
 	}
 	
 }
