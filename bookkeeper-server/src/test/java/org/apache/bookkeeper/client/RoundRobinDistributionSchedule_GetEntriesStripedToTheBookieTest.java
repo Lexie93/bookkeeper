e32527125c98d8aa8cb9fb10792f5ec22669e214
Lexie <ponzo93@gmail.com>
2020-06-27 19:57:04 +0200

    increased condition coverage and removed log messages during tests (finally...)

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/RoundRobinDistributionSchedule_GetEntriesStripedToTheBookieTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/RoundRobinDistributionSchedule_GetEntriesStripedToTheBookieTest.java
@@ -25,21 +25,29 @@ public class RoundRobinDistributionSchedule_GetEntriesStripedToTheBookieTest {
     public static Collection<Object[]> getParameters(){
         Integer[] bIndex = {-1, 0, 2};
         Long[] startId = {-1L, 0L, 1L};
-        Long[] lastId = {0L, 1L, 3L};
-        Integer[] writeQuorum = {2};
-        Integer ackQuorum = 3;
-        Integer[] ensemble = {3};
-        Integer[] card = {2, 3};
+        Long[] lastId = {-1L, 0L, 1L, 3L};
+        Integer[] writeQuorum = {2, 3};
+        int ackQuorum = 3;
+        Integer[] ensemble = {0, 3, 4};
+        Integer[] card = {0, 2, 3};
 
         return Arrays.asList(new Object[][]{
-                {bIndex[1], startId[1], lastId[2], writeQuorum[0], ackQuorum, ensemble[0], card[1], new Boolean[] {true, false, true, true}},
-                {bIndex[2], startId[1], lastId[2], writeQuorum[0], ackQuorum, ensemble[0], card[0], new Boolean[] {false, true, true, false}},
+                {bIndex[1], startId[1], lastId[3], writeQuorum[0], ackQuorum, ensemble[1], card[2], new Boolean[] {true, false, true, true}},
+                {bIndex[2], startId[1], lastId[3], writeQuorum[0], ackQuorum, ensemble[1], card[1], new Boolean[] {false, true, true, false}},
+                {bIndex[1], startId[2], lastId[2], writeQuorum[0], ackQuorum, ensemble[1], card[0], new Boolean[] {false}},
                 //should throw exception (startId<0)
-                {bIndex[2], startId[0], lastId[0], writeQuorum[0], ackQuorum, ensemble[0], card[1], new Boolean[] {}},
+                {bIndex[2], startId[0], lastId[1], writeQuorum[0], ackQuorum, ensemble[1], card[2], new Boolean[] {}},
                 //should throw exception (bIndex<0)
-                {bIndex[0], startId[1], lastId[2], writeQuorum[0], ackQuorum, ensemble[0], card[1], new Boolean[] {}},
+                {bIndex[0], startId[1], lastId[3], writeQuorum[0], ackQuorum, ensemble[1], card[2], new Boolean[] {}},
                 //should throw exception (lastId<startId)
-                {bIndex[1], startId[2], lastId[0], writeQuorum[0], ackQuorum, ensemble[0], card[1], new Boolean[] {}}
+                {bIndex[1], startId[2], lastId[1], writeQuorum[0], ackQuorum, ensemble[1], card[2], new Boolean[] {}},
+                //should throw exception (ensemble<1)
+                {bIndex[1], startId[2], lastId[2], writeQuorum[0], ackQuorum, ensemble[0], card[0], new Boolean[] {}},
+                //added to increase cond coverage, but seems useless...
+                //should throw exception (lastId<0)
+                {bIndex[1], startId[2], lastId[0], writeQuorum[0], ackQuorum, ensemble[1], card[0], new Boolean[] {}},
+                //added to increase cond coverage
+                {bIndex[2], startId[1], lastId[3], writeQuorum[1], ackQuorum, ensemble[2], card[2], new Boolean[] {true, true, true, false}}
         });
     }
 
@@ -66,7 +74,7 @@ public class RoundRobinDistributionSchedule_GetEntriesStripedToTheBookieTest {
                 Assert.assertEquals(result[i], entries.get(i));
             }
         } catch (IllegalArgumentException e){
-            Assert.assertTrue(startEntryId==-1L || bookieIndex==-1 || lastEntryId==0);
+            Assert.assertTrue(startEntryId==-1L || bookieIndex==-1 || lastEntryId==0 || ensembleSize==0 || lastEntryId==-1L);
         } catch (IndexOutOfBoundsException e){
             Assert.fail("Exception not thrown");
         }
